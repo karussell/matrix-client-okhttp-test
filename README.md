901c1fbc9f6f397861e176d84897c7bd3debc461
Peter <graphhopper@gmx.de>
2022-01-27 13:00:54 +0100

    enable parallel requests

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Start three of the following applications in parallel:
 
 ```
 mvn clean install
-java -jar target/matrix-client-test-1.0-SNAPSHOT.jar api_key 3000 true
+java -jar target/matrix-client-test-1.0-SNAPSHOT.jar api_key 3000 true 5
 ```
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,6 +4,8 @@ import com.graphhopper.api.MatrixResponse;
 import com.graphhopper.util.shapes.GHPoint;
 
 import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Test {
     public static void main(String[] args) throws InterruptedException {
@@ -13,22 +15,29 @@ public class Test {
             matrix.setKey(args[0]);
         long sleep = args.length > 1 ? Integer.parseInt(args[1]) : 1000;
         boolean infoLog = args.length > 2 ? Boolean.parseBoolean(args[2]) : false;
-
-        while (true) {
-            if (infoLog)
-                System.out.println(new Date() + " request");
-            GHMRequest req = new GHMRequest();
-            req.addPoint(new GHPoint(51.534377, -0.087891));
-            req.addPoint(new GHPoint(51.467697, -0.090637));
-            try {
-                MatrixResponse rsp = matrix.route(req);
-                if (!rsp.getErrors().isEmpty())
-                    System.err.println(new Date() + " should not happen " + rsp.getErrors());
-            } catch (Exception ex) {
-                // ex.printStackTrace();
-                System.err.println(new Date() + " exception " + ex.getMessage());
-            }
-            Thread.sleep(sleep);
+        int threads = args.length > 3 ? Integer.parseInt(args[3]) : 1;
+        ExecutorService exec = Executors.newFixedThreadPool(threads);
+        for (int i = 0; i < threads; i++) {
+            final int threadIdx = i;
+            exec.submit(() -> {
+                        while (true) {
+                            if (infoLog)
+                                System.out.println("[" + threadIdx + "] " + new Date() + " request");
+                            GHMRequest req = new GHMRequest();
+                            req.addPoint(new GHPoint(51.534377, -0.087891));
+                            req.addPoint(new GHPoint(51.467697, -0.090637));
+                            try {
+                                MatrixResponse rsp = matrix.route(req);
+                                if (!rsp.getErrors().isEmpty())
+                                    System.err.println("[" + threadIdx + "] " + new Date() + " should not happen " + rsp.getErrors());
+                            } catch (Exception ex) {
+                                // ex.printStackTrace();
+                                System.err.println("[" + threadIdx + "] " + new Date() + " exception " + ex.getMessage());
+                            }
+                            Thread.sleep(sleep);
+                        }
+                    }
+            );
         }
     }
 }
